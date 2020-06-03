5512817bd25264b4e3a8858a923db37f03d9e0ce
oguzzerman <oguz.zerman@gmail.com>
2020-06-04 00:33:27 +0300

    Add camera script

---
+++ 2DPlatformGame/Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject MainCharacter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3 (MainCharacter.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
