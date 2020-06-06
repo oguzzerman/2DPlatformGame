f7c6eeddb869055317ee0f0f4463b0216a65470f
Oğuz Zerman <41145421+oguzzerman@users.noreply.github.com>
2020-06-07 00:26:49 +0300

    First build :) First successful webgl deploy

---
+++ 2DPlatformGame/Assets/Scripts/MovementAlwaysRun.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAlwaysRun : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float HorizontalSpeed = 40f;
+    private bool _Jump = false;
+    private bool _Crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _Jump = true;
+            animator.SetBool("Jumping", true);
+        }
+
+
+        if (Input.GetButton("Crouch"))
+        {
+            _Crouch = true;
+
+        }
+        else// if (Input.GetButtonUp("Crouch"))
+        {
+            _Crouch = false;
+        }
+
+
+        animator.SetBool("Crouching", _Crouch);
+    }
+
+    public void OnLanding()
+    {
+        print("landed");
+        animator.SetBool("Jumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(Time.fixedDeltaTime * HorizontalSpeed, _Crouch, _Jump);
+        animator.SetFloat("Speed", Mathf.Abs(HorizontalSpeed));
+        _Jump = false;
+    }
+}
---
+++ 2DPlatformGame/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float HorizontalSpeed = 40f;
+    private float _HorizontalMove;
+    private bool _Jump = false;
+    private bool _Crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _HorizontalMove = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(_HorizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _Jump = true;
+            animator.SetBool("Jumping", true);
+        }
+
+
+        if (Input.GetButton("Crouch"))
+        {
+            _Crouch = true;
+
+        }
+        else// if (Input.GetButtonUp("Crouch"))
+        {
+            _Crouch = false;
+        }
+
+
+        animator.SetBool("Crouching", _Crouch);
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(_HorizontalMove* Time.fixedDeltaTime * HorizontalSpeed, _Crouch, _Jump);
+        _Jump = false;
+    }
+}
