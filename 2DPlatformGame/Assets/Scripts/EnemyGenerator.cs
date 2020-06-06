cee993f52a337966231eb9b587a826af947ea85a
oguzzerman <oguz.zerman@gmail.com>
2020-06-06 19:38:08 +0300

    Add enemy generator script

---
+++ 2DPlatformGame/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public List<GameObject> Enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenerateRandomEnemies();
+
+    }
+
+    private IEnumerator GenerateRandomEnemies()
+    {
+        yield return new WaitForSeconds(5);
+        var rnd = Random.Range(0, Enemies.Count);
+        print(rnd);
+        GameObject enemy = Instantiate(Enemies[rnd]);
+        enemy.active = true;
+
+        
+    }
+}
---
+++ 2DPlatformGame/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+
+        rigidbody2D.velocity = new Vector2(Speed, 0);
+    }
+}
